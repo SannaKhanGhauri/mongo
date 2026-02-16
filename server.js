caa31bcfca8a5e689bc59964018be4ff7f541127
SannaKhanGhauri <sanna.ghauri@hva.nl>
2026-02-16 15:10:23 +0100

    node hide

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-hoi
+const express = require('express'); // Express importeren
+const app = express();
+const PORT = 3000;
+
+
+app.get('/', (req, res) => {
+  res.send('Hallo wereld!');
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server draait op http://localhost:${PORT}`);
+});
